103c7cd4bfbdde6b69e56e7d2a2de47ca7613aa7
GaboManu <715651@student.dusd.net>
2022-11-02 11:44:17 -0700

    Added tiles folder and downloaded png of the FLOOR

--- RubysAdventureVargas/Assets/Scripts/RubyController.cs
+++ RubysAdventureVargas/Assets/Scripts/RubyController.cs
@@ -7,8 +7,7 @@ public class RubyController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        QualitySettings.vSyncCount = 0;
-        Application.targetFrameRate = 120;
+        
     }
 
     // Update is called once per frame
