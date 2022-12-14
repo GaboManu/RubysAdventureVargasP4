2280a68b35c7b8068801c449601221c251928639
GaboManu <715651@student.dusd.net>
2022-12-14 12:22:09 -0800

    Added anims to ruby

--- RubysAdventureVargas/Assets/Scripts/RubyController.cs
+++ RubysAdventureVargas/Assets/Scripts/RubyController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class RubyController : MonoBehaviour
@@ -24,15 +25,14 @@ public class RubyController : MonoBehaviour
     float horizontal;
     float vertical;
 
-    Vector2 move = new Vector2(horizontal, vertical);
-
-
-    if(!Mathf.Approximately(move.x. 0.0f) || !Mathf.Approximately(move.y, 0.0f))
-    {
-        look
+    Animator animator;
+    Vector2 lookDirection = new Vector2(1, 0);
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        
+        
         currentHealth = maxHealth;
     }
 
@@ -42,7 +42,21 @@ public class RubyController : MonoBehaviour
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
 
-        if (isInvincible)
+        Vector2 move = new Vector2(horizontal, vertical);
+
+        if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        {
+            lookDirection.Set(move.x, move.y);
+            lookDirection.Normalize();
+        }
+
+
+        animator.SetFloat("LookX", lookDirection.x);
+        animator.SetFloat("Look Y",lookDirection.y);
+        animator.SetFloat("Speed", move.magnitude);
+
+            
+            if (isInvincible)
         {
             invincibleTimer -= Time.deltaTime;
             if (invincibleTimer < 0)
@@ -64,6 +78,7 @@ public class RubyController : MonoBehaviour
     {
         if (amount < 0)
         {
+            animator.SetTrigger("Hit");
             if (isInvincible)
                 return;
 
